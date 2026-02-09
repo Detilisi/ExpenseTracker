25c380aedb53b13aa86015620b20c8ede1b42de8
detilisi <bdetilisi@gmail.com>
2026-02-09 22:09:40 +0200

    refactor : Specify entity id type

--- src/ExpenseTracker.Domain/Entities/UserEntity.cs
+++ src/ExpenseTracker.Domain/Entities/UserEntity.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace ExpenseTracker.Domain.Entities;
 
-public class UserEntity : IdentityUser
+public class UserEntity : IdentityUser<int>
 {
     public required string FirstName { get; set; }
     public required string LastName { get; set; }
--- src/ExpenseTracker.Infrastructure/Persistence/ApplicationDataContext.cs
+++ src/ExpenseTracker.Infrastructure/Persistence/ApplicationDataContext.cs
@@ -1,10 +1,11 @@
 using ExpenseTracker.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ExpenseTracker.Infrastructure.Persistence;
 
-public class ApplicationDataContext :  IdentityDbContext<UserEntity>
+public class ApplicationDataContext :  IdentityDbContext<UserEntity, IdentityRole<int>, int>
 {
     public DbSet<UserEntity> UserEntities { get; set; }
     public DbSet<CategoryEntity> CategoriesEntities { get; set; }  
